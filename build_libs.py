54637523b2643b7bfb59ec250f84f41fff3394db
serengeor <serengeor@gmail.com>
2014-09-14 19:13:51 -0700

    build script improvements: can build boost if it exists in libs/boost

--- build_libs.py
+++ build_libs.py
@@ -25,10 +25,38 @@ else:
 libpath = os.path.join("libs", platform);
 fullLibPath = os.path.join(os.getcwd(), libpath)
 
+#COMPILE BOOST
+
 #warn if no boost
 boostpath = os.path.join(os.getcwd(), "libs/boost")
 if (os.path.isdir(boostpath) == False) or (len(os.listdir(boostpath)) == 0):
     easygui.msgbox("You do not have boost sources inside libs folder.\nClose this message box to continue.", title="Boost missing!")
+else:
+    if easygui.boolbox(msg='Would you like to compile boost', title='Compile boost', choices=('Yes', 'No'), image=None):
+        restore_path = os.getcwd()
+        
+        os.chdir(boostpath)
+        print("Calling boost: bootstrap gcc")
+        subprocess.call('bootstrap gcc', shell=True)
+        
+        choices = ["1", "4", "8"]
+        choice = easygui.choicebox("Choose number of cores available", "Core choice", choices)
+
+        buildstr = "b2 "
+
+        if choice != None :
+            if choice == "4":
+                buildstr += "-j5 "
+            elif choice == "8":
+                buildstr += "-j9 "
+
+        buildstr += "toolset=gcc link=static threading=multi release stage"
+        
+        print("Calling boost: " + buildstr)
+        subprocess.call(buildstr, shell=True)
+        
+        os.chdir(restore_path)
+
 
 #COMPILE
 if os.path.isdir("build") == False:
