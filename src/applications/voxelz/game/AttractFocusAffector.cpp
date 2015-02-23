feefd2d9b8ea3da4521a94a242a5819a4a069b04
Andrius Tarasovas <andrius.tarasovas@visma.com>
2015-02-23 14:41:41 +0200

    I don't know what I am doing.

--- src/applications/voxelz/game/AttractFocusAffector.cpp
+++ src/applications/voxelz/game/AttractFocusAffector.cpp
@@ -9,9 +9,18 @@ AttractFocusAffector::AttractFocusAffector(const glm::vec3 &attractionPoint,floa
     _power=power;
 }
 
+static glm::vec3 calc_force(glm::vec3 a, glm::vec3 b)
+{
+    glm::vec3 r = a-b;
+    float r_mag = glm::length(r);
+    glm::vec3 r_norm = glm::normalize(r);
+    glm::vec3 ret = ((float)r_mag) * -r_norm;
+    return ret;
+}
+
 void AttractFocusAffector::Affect(float dt,Particle &particleToAffect)
 {
-    float dist=glm::distance(_attractionPoint,particleToAffect.pos);
-    glm::vec3 dir=glm::normalize(_attractionPoint-particleToAffect.pos);
-    particleToAffect.pos+=particleToAffect.speed*_power*dt*-dir*200.f/dist;
+//    float dist=glm::distance(_attractionPoint,particleToAffect.pos);
+//    glm::vec3 dir=glm::normalize(_attractionPoint-particleToAffect.pos);
+    particleToAffect.pos+=calc_force(_attractionPoint,particleToAffect.pos)*dt;
 }
