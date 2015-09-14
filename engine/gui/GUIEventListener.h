e96a1e4461fa7f5da4719b68075acd877f712548
serengeor <serengeor@gmail.com>
2015-09-15 00:26:48 +0300

    Removed old engine code

--- engine/gui/GUIEventListener.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GUIEvent.h"
-
-class GUIEventListener
-{
-public:
-    virtual ~GUIEventListener() {}
-    virtual bool OnEvent(const GUIEvent & e)=0;
-};
--- engine/resources/IMeshLoader.cpp
+++
@@ -1,9 +0,0 @@
-#include "Precomp.h"
-#include "IMeshLoader.h"
-#include "opengl/Mesh.h"
-#include "opengl/BufferObject.h"
-
-imesh_loader::~imesh_loader()
-{
-    //dtor
-}
