52e43bdcba4ce7ec76043849aaf168c307817b81
serengeor <serengeor@gmail.com>
2015-09-17 17:41:44 +0300

    Made application build, need those god damn resources tho.

--- voxel_octree/run.sh
+++ voxel_octree/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ./bin
-export LD_LIBRARY_PATH=/home/serengeor/Coding/Project/src/engine/build:$LD_LIBRARY_PATH
-exec ./VoxelOctree $1
+export LD_LIBRARY_PATH=/home/serengeor/Coding/Project/bin/Debug:$LD_LIBRARY_PATH
+exec optirun ./VoxelOctree $1
