e834fb7e6e6dcb60b9dbf3d0171af4330bc6907f
serengeor <serengeor@gmail.com>
2015-10-16 20:02:18 +0300

    Removed old unused files. Voxel export script is now in new repo: https://github.com/serengeor/BlenderVoxelExporter

--- voxel_octree/tools/blender/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
- 
-module1 = Extension('tri_aabb', sources = ['tri_aabb.cpp'])
- 
-setup (name = 'TriAABB',
-        version = '1.0',
-        description = 'This is a fully featured voxelizer package!',
-	ext_modules = [module1] )
